0ac71f01944f24ea8d1f626970f8a75c0c0a3417
John Agapeyev <jagapeyev@gmail.com>
2017-12-02 17:03:11 -0800

    Changing stuff

--- src/client/network.c
+++ src/client/network.c
@@ -105,7 +105,7 @@ bool ackReceived = false;
 #define TIMEOUT_NS 1000ul * MICRO_IN_SEC
 #define MAX_RETRIES 30
 
-#define ACK_DELAY_NS 10ul * MICRO_IN_SEC
+#define ACK_DELAY_NS 50ul * MICRO_IN_SEC
 
 struct timespec timeToWait;
 
